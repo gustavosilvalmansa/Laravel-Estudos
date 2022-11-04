18ba08b1d1dc51dbbb9706ed37a61a7d21a969bb
Gustavo Almansa <gustavosalmansa@gmail.com>
2022-11-04 17:42:14 -0300

    Route redirect

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,13 @@ Route::get('/categoria/{nome}/{categoria_id}', function(String $nome = "Unkown",
     echo 'Categoria ID '.$categoria_id."<br>";
 })->where('categoria_id','[0-9]+')->where('nome','[A-Za-z]+');
 
+
+Route::get('/rota1', function(){
+    echo 'rota 1';
+})->name('site.rota1');
+
+
+Route::get('/rota2', function(){
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
