97cdb209f8334de4e3fb78bc4bfa21c4c71e3db2
Gustavo Almansa <gustavosalmansa@gmail.com>
2022-11-08 15:08:30 -0300

    @if/else BLADE

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,7 +1,16 @@
-<h3> teste </h3>
-
 {{-- Comentario --}}
 
+
 @php
-    echo "teste";
+   //Bloco puro de PHP
 @endphp 
+
+{{--@dd($fornecedores)--}}
+
+@if(count($fornecedores) > 0  && count($fornecedores)<10)
+    <h1>Menos de 10</h1>
+@elseif(count($fornecedores) >10)
+    <h3> Mais de 10 </h3>
+@else
+    <h3>Nenhum fornecedor</h3>
+@endif
