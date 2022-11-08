182ecf824fab6bc9a222c7ab4f2fe441cda2616c
Gustavo Almansa <gustavosalmansa@gmail.com>
2022-11-08 14:33:42 -0300

    Adicionando comentarios e blocos de PHP Puro

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,7 @@
+<h3> teste </h3>
+
+{{-- Comentario --}}
+
+@php
+    echo "teste";
+@endphp 
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::get('/sobrenos', 'SobreNosController@sobreNos')->name('site.sobrenos');
@@ -21,7 +10,7 @@ Route::get('/login', function(){return "login";})->name('site.login');
 
 Route::prefix('/app')->group(function(){
     Route::get('/clientes', function(){return "clientes";})->name('app.clientes');
-    Route::get('/fornecedores', function(){return "fornecedores";})->name('app.fornecedores');
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
     Route::get('/produtos', function(){return "produtos";})->name('app.produtos');
 });
 
@@ -35,7 +24,7 @@ Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
 
 
 Route::get('/redir1', function(){
-    echo 'rota 1';
+    echo 'Rota 1';
 })->name('site.redir1');
 
 
